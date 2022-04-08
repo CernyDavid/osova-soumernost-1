02ccb869e5b64797dfcb6748b90090aa120d5285
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-04-08 15:42:18 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,21 @@
+led.plot_brightness(0, 2, 100)
+led.plot_brightness(1, 2, 100)
+led.plot_brightness(2, 2, 100)
+led.plot_brightness(3, 2, 100)
+led.plot_brightness(4, 2, 100)
+a = 1
+b = 0
+c = 1
+d = 1
+e = 2
+f = 0
+g = 2
+h = 1
+led.plot(a, b)
+led.plot(c, d)
+led.plot(e, f)
+led.plot(g, h)
+led.plot(a, 2 + (2 - b))
+led.plot(c, 2 + (2 - d))
+led.plot(e, 2 + (2 - f))
+led.plot(g, 2 + (2 - h))
---
+++ main.ts
@@ -0,0 +1,21 @@
+led.plotBrightness(0, 2, 100)
+led.plotBrightness(1, 2, 100)
+led.plotBrightness(2, 2, 100)
+led.plotBrightness(3, 2, 100)
+led.plotBrightness(4, 2, 100)
+let a = 1
+let b = 0
+let c = 1
+let d = 1
+let e = 2
+let f = 0
+let g = 2
+let h = 1
+led.plot(a, b)
+led.plot(c, d)
+led.plot(e, f)
+led.plot(g, h)
+led.plot(a, 2 + (2 - b))
+led.plot(c, 2 + (2 - d))
+led.plot(e, 2 + (2 - f))
+led.plot(g, 2 + (2 - h))
