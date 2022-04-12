b7d48330b15166c22953f64b584882e805bb9ddc
David Černý <91247706+CernyDavid@users.noreply.github.com>
2022-04-12 10:53:19 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ led.plot_brightness(1, 2, 100)
 led.plot_brightness(2, 2, 100)
 led.plot_brightness(3, 2, 100)
 led.plot_brightness(4, 2, 100)
-a = 1
-b = 0
-c = 1
-d = 1
-e = 2
-f = 0
-g = 2
-h = 1
-led.plot(a, b)
-led.plot(c, d)
-led.plot(e, f)
-led.plot(g, h)
-led.plot(a, 2 + (2 - b))
-led.plot(c, 2 + (2 - d))
-led.plot(e, 2 + (2 - f))
-led.plot(g, 2 + (2 - h))
+a = [1, 0]
+b = [1, 1]
+c = [2, 0]
+d = [2, 1]
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+led.plot(c[0], c[1])
+led.plot(d[0], d[1])
+led.plot(a[0], 2 + (2 - a[1]))
+led.plot(b[0], 2 + (2 - b[1]))
+led.plot(c[0], 2 + (2 - c[1]))
+led.plot(d[0], 2 + (2 - d[1]))
--- main.ts
+++ main.ts
@@ -3,19 +3,15 @@ led.plotBrightness(1, 2, 100)
 led.plotBrightness(2, 2, 100)
 led.plotBrightness(3, 2, 100)
 led.plotBrightness(4, 2, 100)
-let a = 1
-let b = 0
-let c = 1
-let d = 1
-let e = 2
-let f = 0
-let g = 2
-let h = 1
-led.plot(a, b)
-led.plot(c, d)
-led.plot(e, f)
-led.plot(g, h)
-led.plot(a, 2 + (2 - b))
-led.plot(c, 2 + (2 - d))
-led.plot(e, 2 + (2 - f))
-led.plot(g, 2 + (2 - h))
+let a = [1, 0]
+let b = [1, 1]
+let c = [2, 0]
+let d = [2, 1]
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+led.plot(c[0], c[1])
+led.plot(d[0], d[1])
+led.plot(a[0], 2 + (2 - a[1]))
+led.plot(b[0], 2 + (2 - b[1]))
+led.plot(c[0], 2 + (2 - c[1]))
+led.plot(d[0], 2 + (2 - d[1]))
